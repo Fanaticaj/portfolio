d82b8ba34b87af6b7121f3fd541bca0de712d401
Anthony Grippi <anthonyjgrippi@gmail.com>
2025-03-11 14:19:13 -0700

    began implemening CarouselItem

---
+++ portfolio/src/components/CarouselItem.tsx
@@ -0,0 +1,5 @@
+export function CarouselItem() {
+  return <div className=""></div>;
+}
+
+export default CarouselItem;
--- portfolio/src/components/SlideShow.tsx
+++ portfolio/src/components/SlideShow.tsx
@@ -1,13 +1,14 @@
 "use client";
 
 import { Carousel } from "flowbite-react";
+import CarouselItem from "./CarouselItem";
 //
 export function SlideShow() {
   return (
     <div className="h-[70vh] w-[70vw]">
       <Carousel>
         <div className="flex h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">
-          Slide 1
+          <CarouselItem />
         </div>
         <div className="flex h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">
           Slide 2

40234fbf73b3713aa6b40c59674b80d7ce0d822c
Anthony Grippi <anthonyjgrippi@gmail.com>
2025-05-09 15:21:22 -0700

    removed amplify stuff

--- portfolio/amplify-testing/backend.ts
+++
@@ -1,8 +0,0 @@
-import { defineBackend } from '@aws-amplify/backend';
-import { data } from './data/resource.js';
-import { storage } from './storage/resource';
-
-defineBackend({
-  data,
-  storage
-});
